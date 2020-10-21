74880f2fb49285fec5a61568360ce8b2e651c87a
pra11chit <pratikchitode@gmail.com>
2020-10-21 16:37:02 +0530

    basic done

--- psh.c
+++ psh.c
@@ -1,5 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/* shell built-ins forward declaration */
+int psh_cd(char **args);
+int psh_help(char **args);
+int psh_exit(char **args);
+
+/* */
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_func[])(char **) = {
+    &psh_cd,
+    &psh_help,
+    &psh_exit
+};
+
+/*      shell built-ins implementation     */
+int psh_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "psh : expected argument to \"cd\"\n");
+    }
+    else {
+        if (chdir(args[1]) != 0) {
+            perror("psh");
+        }
+    }
+    return 1;
+}
+
+int psh_exit(char **args) {
+    return 0;
+}
+
+int psh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int psh_help(char **args) {
+    printf("Welcome to Pratik's psh shell\n");
+    printf("Following are builtins : \n");
+    for (int i = 0; i < psh_num_builtins(); i++) {
+        printf(" %s\n", builtin_str[i]);
+    }
+}
 
 #define PSH_RL_BUFSIZE 1024
 char *psh_read_line() {
@@ -55,19 +105,53 @@ char **psh_split_line(char *line) {
             bufsize += PSH_TOK_BUFSIZE;
             tokens = realloc(tokens, bufsize * sizeof(char*));
             if (!tokens) {
-            fprintf(stderr, "lsh : allocation failed\n");
+            fprintf(stderr, "psh : allocation failed\n");
             exit(EXIT_FAILURE);
             }
         }
+        token = strtok(NULL, PSH_TOK_DELIM);
     }
     tokens[position] = NULL;
     return tokens;
 }
 
 int psh_launch(char **args) {
+    pid_t pid, wpid;
+    int status;
+    pid = fork();
+
+    if (pid == 0) {
+        //child process
+        if (execvp(args[0], args) == -1) {
+            perror("psh");
+        }   exit(EXIT_FAILURE);
+    }
+    else if (pid < 0) {
+        perror("psh");
+    }
+    else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    
+    return 1;
 }
 
-char *psh_loop() {
+int psh_execute(char **args) {
+    if (args[0] == NULL) {
+        return 1;
+    }
+
+    for (int i = 0; i < psh_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+    return psh_launch(args);
+}
+
+void psh_loop() {
     char *line; //input
     char **args; //array of split arguments
     int status;
@@ -76,10 +160,10 @@ char *psh_loop() {
         printf("> ");
         line = psh_read_line();
         args = psh_split_line(line);
-        //status = psh_execute(args);
+        status = psh_execute(args);
 
-        //free(line);
-        //free(args);
+        free(line);
+        free(args);
     } while(status);
 }
 
